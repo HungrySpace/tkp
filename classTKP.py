173aaedd08c3422ce76bccfb8cdb34bab9413352
Kirill <mikirus46@gmail.com>
2021-01-08 19:23:44 +0300

    добавил шмон по папкам для поиска всех excel файлов в структуре папок

--- classTKP.py
+++ classTKP.py
@@ -9,21 +9,21 @@ class TKP:
         # выбор первого листа докумета
         sheet = book.active
         self.dict_value_file = {"number_tkp": str(sheet['B1'].value).zfill(4),
-                           "gip_tkp": sheet['B2'].value,
-                           "name_tkp": sheet['D1'].value,
-                           "object_tkp": sheet['D2'].value,
-                           "brand_1": sheet['A8'].value,
-                           "brand_2": sheet['B8'].value,
-                           "brand_3": sheet['C8'].value,
-                           "brand_4": sheet['D8'].value,
-                           "brand_5": sheet['E8'].value,
-                           "brand_6": sheet['F8'].value,
-                           "brand_7": sheet['G8'].value,
-                           "brand_8": sheet['H8'].value,
-                           "brand_9": sheet['I8'].value,
-                           "brand_10": sheet['J8'].value,
-                           "status_stp_1C": sheet['A11'].value,
-                           "status_stp": sheet['B11'].value}
+                                "gip_tkp": sheet['B2'].value,
+                                "name_tkp": sheet['D1'].value,
+                                "object_tkp": sheet['D2'].value,
+                                "brand_1": sheet['A8'].value,
+                                "brand_2": sheet['B8'].value,
+                                "brand_3": sheet['C8'].value,
+                                "brand_4": sheet['D8'].value,
+                                "brand_5": sheet['E8'].value,
+                                "brand_6": sheet['F8'].value,
+                                "brand_7": sheet['G8'].value,
+                                "brand_8": sheet['H8'].value,
+                                "brand_9": sheet['I8'].value,
+                                "brand_10": sheet['J8'].value,
+                                "status_stp_1C": sheet['A11'].value,
+                                "status_stp": sheet['B11'].value}
 
     def print_val(self):
         print(self.dict_value_file)
--- puk_2.py
+++ puk_2.py
@@ -8,19 +8,27 @@ os.chdir(main_folder)
 # for i in range(5):
 #     os.mkdir(str(i).zfill(4))
 
-# for i in os.listdir():
-#     print(i)
-
+for i in os.listdir():
+    # сбор путей файлов ткп
+    path_tut = main_folder + '\\' + i
+    os.chdir(path_tut)
+    # print(path_tut)
+    for file_tkp in os.listdir():
+        if file_tkp.find("Карточка ТКП") >= 0:
+            path_file = path_tut + '\\' + file_tkp
+            print(path_file)
+        else:
+            print('Левый файл в папке:', file_tkp)
 
 # сбор путей файлов ткп
-path_tut = main_folder + '\\' + os.listdir()[0]
-os.chdir(path_tut)
-path_tkp = path_tut + '\\' + os.listdir()[0]
-number_tkp = path_tkp.split('\\')[-2]
-print(path_tkp, "//", number_tkp)
-
-one_file = classTKP.TKP(path_tkp, number_tkp)
-one_file.print_val()
+# path_tut = main_folder + '\\' + os.listdir()[0]
+# os.chdir(path_tut)
+# path_tkp = path_tut + '\\' + os.listdir()[0]
+# number_tkp = path_tkp.split('\\')[-2]
+# print(path_tkp, "//", number_tkp)
+#
+# one_file = classTKP.TKP(path_tkp, number_tkp)
+# one_file.print_val()
 
 
 
