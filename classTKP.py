b671e4d409850f0f0b1733cc65b38afd56693fd1
Kirill <mikirus46@gmail.com>
2021-01-09 00:32:40 +0300

    Прикрутил создаение структуры папок с гип'ами и ярлики на папки с файлими excel

--- classTKP.py
+++ classTKP.py
@@ -1,29 +1,65 @@
 import openpyxl
+import os, winshell
+from win32com.client import Dispatch
+
+
 
 
 class TKP:
-    def __init__(self, path_tkp_file, number_tkp):
+    def __init__(self, path_tkp_file, number_tkp, main_folder):
+        self.main_folder = main_folder
         self.path_tkp_file = path_tkp_file
         self.number_tkp = number_tkp
         book = openpyxl.open(path_tkp_file, read_only=True)
         # выбор первого листа докумета
         sheet = book.active
-        self.dict_value_file = {"number_tkp": str(sheet['B1'].value).zfill(4),
-                                "gip_tkp": sheet['B2'].value,
-                                "name_tkp": sheet['D1'].value,
-                                "object_tkp": sheet['D2'].value,
-                                "brand_1": sheet['A8'].value,
-                                "brand_2": sheet['B8'].value,
-                                "brand_3": sheet['C8'].value,
-                                "brand_4": sheet['D8'].value,
-                                "brand_5": sheet['E8'].value,
-                                "brand_6": sheet['F8'].value,
-                                "brand_7": sheet['G8'].value,
-                                "brand_8": sheet['H8'].value,
-                                "brand_9": sheet['I8'].value,
-                                "brand_10": sheet['J8'].value,
-                                "status_stp_1C": sheet['A11'].value,
-                                "status_stp": sheet['B11'].value}
+        self.dict_value_file = {"Номер ТКП": str(sheet['B1'].value).zfill(4),
+                                "ГИП": sheet['B2'].value,
+                                "Краткое наименование ТКП": sheet['D1'].value,
+                                "Объект": sheet['D2'].value,
+                                "Бренд 1": sheet['A8'].value,
+                                "Бренд 2": sheet['B8'].value,
+                                "Бренд 3": sheet['C8'].value,
+                                "Бренд 4": sheet['D8'].value,
+                                "Бренд 5": sheet['E8'].value,
+                                "Бренд 6": sheet['F8'].value,
+                                "Бренд 7": sheet['G8'].value,
+                                "Бренд 8": sheet['H8'].value,
+                                "Бренд 9": sheet['I8'].value,
+                                "Бренд 10": sheet['J8'].value,
+                                "Статус ТКП 1С": sheet['A11'].value,
+                                "Статус ТКП в отделе": sheet['B11'].value,
+                                "Название направления 1": sheet['H1'].value,
+                                "Название направления 2": sheet['H2'].value,
+                                "Название направления 3": sheet['J1'].value,
+                                "Название направления 4": sheet['J1'].value,
+                                }
+        pass
 
     def print_val(self):
         print(self.dict_value_file)
+        pass
+
+    def gip_shortcut(self):
+        path_main_gip = str(self.main_folder) + '\\' + 'GIP'
+        # создаем папку если такой нет
+        if not os.path.isdir(path_main_gip):
+            os.mkdir(path_main_gip)
+        if str(self.dict_value_file.get('ГИП')) != 'None':
+            path_gip = path_main_gip + '\\' + str(self.dict_value_file.get('ГИП'))
+            if not os.path.isdir(path_gip):
+                os.mkdir(path_gip)
+            # путь где ярлык будет создан
+            desktop = path_gip
+            # создаем сам ярлык
+            path = os.path.join(desktop, str(self.number_tkp)+".lnk")
+            target = self.main_folder + '\\' + str(self.number_tkp)
+            # wDir = ''
+            # icon = path
+            shell = Dispatch('WScript.Shell')
+            shortcut = shell.CreateShortCut(path)
+            shortcut.Targetpath = target
+            # shortcut.WorkingDirectory = wDir
+            # shortcut.IconLocation = icon
+            shortcut.save()
+        pass
--- puk_2.py
+++ puk_2.py
@@ -9,18 +9,20 @@ for i in os.listdir():
     # сбор путей файлов ткп
     path_tut = main_folder + '\\' + i
     os.chdir(path_tut)
-    # print(path_tut)
+    # print(path_tut[1])
     for file_tkp in os.listdir():
-        if file_tkp.find("Карточка ТКП") >= 0:
+        if file_tkp.find("Карточка ТКП") >= 0 and file_tkp[0] != '~':
             path_file = path_tut + '\\' + file_tkp
             number_tkp = path_file.split('\\')[-2]
-            one_file = classTKP.TKP(path_file, number_tkp)
+            one_file = classTKP.TKP(path_file, number_tkp, main_folder)
             list_class_tkp.append(one_file)
-        else:
+        elif file_tkp.find("Карточка ТКП") == -1 and file_tkp[0] != '~':
             print('Левый файл в папке:', file_tkp)
+        elif file_tkp[0] == '~':
+            print('наткнулись на открытый файл')
 
 for puk in list_class_tkp:
-    puk.print_val()
+    puk.gip_shortcut()
 
 # # создаем папки в деректории
 # for i in range(5):
